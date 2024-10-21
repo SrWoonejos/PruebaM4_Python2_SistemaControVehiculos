09e3560bc181f18718084e3ea344f212bb7dc521
SrWoonejos <danmirandam@gmail.com>
2024-10-21 18:38:06 -0300

    update class Automovil

--- Parte1/automovil.py
+++ Parte1/automovil.py
@@ -3,5 +3,25 @@
 from vehiculo import Vehiculo
 
 class Automovil(Vehiculo):
-    def __init__(self, marca: str, modelo: str, nro_ruedas: int):
+    def __init__(self, marca: str, modelo: str, nro_ruedas: int, velocidad: float, cilindrada: int):
         super().__init__(marca, modelo, nro_ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    #getters
+    def get_velocidad(self):
+        return self.velocidad
+    
+    def get_cilindrada(self):
+        return self.cilindrada
+    
+    #setters
+    def set_velocidad(self):
+        return self.velocidad
+
+    def set_cilindrada(self):
+        return self.cilindrada
+    
+    def __str__(self):
+        base_info = super().__str__() 
+        return f"{base_info}, Velocidad: {self.velocidad} km/h, Cilindrada: {self.cilindrada} cc"
